0855485de52c849a921cb612f3fcb44936bab4db
divinejoshua <divineuche399@gmail.com>
2023-09-10 09:15:19 +0100

    Completed user registration project

--- userservice/accounts/views.py
+++ userservice/accounts/views.py
@@ -20,7 +20,7 @@ class RegisterView(generics.CreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        # self.perform_create(serializer)
 
         # Procuder 
         producerUserCreated.publish("user_created_method",json.dumps(serializer.data))
